9a312b02296ee64c999f7734942a5a55766a73e7
msiharath <30591101+msiharath@users.noreply.github.com>
2017-08-03 10:04:53 +0100

    Created questions for quiz

--- quiz.js
+++ quiz.js
@@ -3,40 +3,40 @@ var quiztitle = "Social Security Quiz";
 
 var quiz = [
        {
-           "question" : "Q1: Who came up with the theory of relativity?",
-           "image" : "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/220px-Albert_Einstein_Head.jpg",
-           "choices" : [
-                                   "Sir Isaac Newton",
-                                   "Nicolaus Copernicus",
-                                   "Albert Einstein",
-                                   "Ralph Waldo Emmerson"
-                               ],
-           "correct" : "Albert Einstein",
-           "explanation" : "Albert Einstein drafted the special theory of relativity in 1905.",
-       },
-       {
-           "question" : "Q2: Who is on the two dollar bill?",
-           "image" : "http://upload.wikimedia.org/wikipedia/commons/thumb/9/94/US_%242_obverse-high.jpg/320px-US_%242_obverse-high.jpg",
-           "choices" : [
-                                   "Thomas Jefferson",
-                                   "Dwight D. Eisenhower",
-                                   "Benjamin Franklin",
-                                   "Abraham Lincoln"
-                               ],
-           "correct" : "Thomas Jefferson",
-           "explanation" : "The two dollar bill is seldom seen in circulation. As a result, some businesses are confused when presented with the note.",
-       },
-       {
-           "question" : "Q3: What event began on April 12, 1861?",
+           "question" : "Q1: 2 + 3?",
            "image" : "",
            "choices" : [
-                                   "First manned flight",
-                                   "California became a state",
-                                   "American Civil War began",
-                                   "Declaration of Independence"
+                                   "6",
+                                   "5",
+                                   "1",
+                                   "4"
                                ],
-           "correct" : "American Civil War began",
-           "explanation" : "South Carolina came under attack when Confederate soldiers attacked Fort Sumter. The war lasted until April 9th 1865.",
+           "correct" : "5",
+           "explanation" : "Practise makes perfect!",
+       },
+       {
+           "question" : "Q2: 2 + 5 * 2?",
+           "image" : "",
+           "choices" : [
+                                   "8",
+                                   "14",
+                                   "9",
+                                   "12"
+                               ],
+           "correct" : "12",
+           "explanation" : "Don't forget to do multiplication before addition!",
+       },
+       {
+           "question" : "Q3: 5 - 2 + 8 % 4?",
+           "image" : "",
+           "choices" : [
+                                   "3",
+                                   "5",
+                                   "11",
+                                   "4"
+                               ],
+           "correct" : "5",
+           "explanation" : "Did you remember BODMAS? Click on next question to find out your score!",
        },
 
 
